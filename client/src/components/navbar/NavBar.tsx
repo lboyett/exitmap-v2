311f2a449a8a0a0d082a98596a1e7bc1d8de1555
Luke Boyett <lboyett@gmail.com>
2023-02-23 11:30:51 +0100

    add navbar dropdown for mobile

--- client/src/components/navbar/NavBar.tsx
+++ client/src/components/navbar/NavBar.tsx
@@ -1,37 +1,80 @@
 import "./navbar.css";
-import { Box, Flex, Heading, Spacer, HStack } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Heading,
+  Spacer,
+  HStack,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+  Button,
+  IconButton,
+} from "@chakra-ui/react";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useNavigate } from "react-router-dom";
 
-interface NavBarProps extends React.HTMLAttributes<HTMLDivElement> {
-}
+interface NavBarProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export default function NavBar(props: NavBarProps) {
   const navigate = useNavigate();
 
   return (
     <Flex className="navbar" bg="bg.500">
-      <Heading className="logo" as="h1" color="txt.500" onClick={() => navigate('/home')}>
+      <Heading
+        className="logo"
+        as="h1"
+        color="txt.500"
+        onClick={() => navigate("/home")}
+      >
         ExitMap
       </Heading>
       <HStack className="navbar-links">
-        <Box className="navbar-icon" color="txt.500">
-          <FontAwesomeIcon icon={faBars} size={"xl"} />
-        </Box>
+        <Menu>
+          <Box color="txt.500" className="navbar-icon">
+            <MenuButton
+              as={IconButton}
+              icon={<FontAwesomeIcon icon={faBars} size={"xl"} />}
+            >
+              Actions
+            </MenuButton>
+          </Box>
+          <MenuList
+            className="navbar-menu-list"
+            bg="bg.500"
+            color="txt.500"
+            borderColor="txt.500"
+          >
+            <MenuItem bg="bg.500" onClick={() => navigate("/home")}>
+              Home
+            </MenuItem>
+            <MenuItem bg="bg.500" onClick={() => navigate("/countries")}>
+              Exits
+            </MenuItem>
+            <MenuItem bg="bg.500" onClick={() => navigate("/submit")}>
+              Submit Exit
+            </MenuItem>
+          </MenuList>
+        </Menu>
         <Heading
           as="h2"
           color="txt.500"
           borderBottom="1px solid black"
           borderBottomColor="txt.500"
-          onClick={() => navigate('/home')}
+          onClick={() => navigate("/home")}
         >
           Home
         </Heading>
-        <Heading as="h2" color="txt.500" onClick={() => navigate('/countries')}>
+        <Heading as="h2" color="txt.500" onClick={() => navigate("/countries")}>
           Exits
         </Heading>
-        <Heading as="h2" color="txt.500" onClick={() => navigate('/submit')}>
+        <Heading as="h2" color="txt.500" onClick={() => navigate("/submit")}>
           Submit Exit
         </Heading>
       </HStack>
