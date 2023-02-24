3f671f42a451b623e68dfa0e88856768fb65b01c
jacksonboyett <jackson.boyett@gmail.com>
2023-02-24 10:57:56 +0100

    updated country page

--- client/src/components/exit-card/ExitCard.tsx
+++ client/src/components/exit-card/ExitCard.tsx
@@ -14,6 +14,7 @@ import { useNavigate } from "react-router-dom";
 import countryImage from "../../assets/country-image.jpeg";
 import "./exit-card.css";
 import { FaHiking } from "react-icons/fa";
+import { useState } from "react";
 
 interface ExitCardProps {
   name: string;
@@ -25,12 +26,25 @@ interface ExitCardProps {
 
 function ExitCard(props: ExitCardProps) {
   const navigate = useNavigate();
+  const [legalityColor, setLegalityColor] = useState('green')
+
 
   const txt_300 = useColorModeValue("txt_light.300", "txt_dark.300");
   const txt_500 = useColorModeValue("txt_light.500", "txt_dark.500");
   const bg_500 = useColorModeValue("bg_light.500", "bg_dark.500");
   const out_500 = useColorModeValue("out_light.500", "out_dark.500");
   const lightMode = useColorModeValue(true, false);
+
+  function legalityColorPicker() {
+    switch (props.legality) {
+      case 'legal': 
+        return 'lime';
+      case 'legalish': 
+        return 'yellow';
+      case 'illegal': 
+        return 'red'
+    }
+  }
 
   return (
     <div>
@@ -49,19 +63,19 @@ function ExitCard(props: ExitCardProps) {
         <Grid
           className="exit-card-grid"
           templateColumns="3fr 1fr"
-          templateRows={"1fr 1fr 1fr"}
+          templateRows={"1fr 1fr 0.5fr"}
         >
           <Heading as="h4">{props.name}</Heading>
-          <Text className="exit-card-legality">{props.legality}</Text>
+          <Text className="exit-card-legality" style={{color: legalityColorPicker()}} bg='black '>{props.legality}</Text>
           <Text className="exit-card-description">{props.description}</Text>
           <Spacer />
           <Text className="exit-card-height">{props.height} ft</Text>
-          <Box>
+          <Flex className="exit-card-hiking">
             <FaHiking className="exit-card-hiking-icon" />
             <Text className="exit-card-hiking-time">
               {props.hikingTime} hrs
             </Text>
-          </Box>
+          </Flex>
         </Grid>
       </ListItem>
     </div>
