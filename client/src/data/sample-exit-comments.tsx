d5d2c59ebc1a40462c021808f77180e1b5840d70
jacksonboyett <jackson.boyett@gmail.com>
2023-02-27 11:40:38 +0100

    changed map height on mobile to allow proper scrolling

--- client/src/data/sample-exit-comments.tsx
+++ client/src/data/sample-exit-comments.tsx
@@ -4,24 +4,24 @@ export const exitComments = [
 	text: 'This jump is amazing!',
 	created_at: 'January 12, 2023',
 },
-// {
-// 	username: 'lboyett14',
-// 	text: 'I went on this jump with my twin brother in 2020. I ended up throwing up on the hike up lol. I was being kind of a pussy. ',
-// 	created_at: 'January 12, 2023',
-// },
-// {
-// 	username: 'jacksonboyett',
-// 	text: 'This jump is amazing!',
-// 	created_at: 'January 12, 2023',
-// },
-// {
-// 	username: 'dickhansen',
-// 	text: 'I did this jump back in 2020 with the twins, a lot of people in the BASE community know exactly who I am talking about lmfao. I hate hiking, but I have to say that this hike was actually pretty rad. The jump was amazing too, it was beautiful and  a very long flight!',
-// 	created_at: 'January 12, 2023',
-// },
-// {
-// 	username: 'jacksonboyett',
-// 	text: 'This jump is amazing!',
-// 	created_at: 'January 12, 2023',
-// }
+{
+	username: 'lboyett14',
+	text: 'I went on this jump with my twin brother in 2020. I ended up throwing up on the hike up lol. I was being kind of a pussy. ',
+	created_at: 'January 12, 2023',
+},
+{
+	username: 'jacksonboyett',
+	text: 'This jump is amazing!',
+	created_at: 'January 12, 2023',
+},
+{
+	username: 'dickhansen',
+	text: 'I did this jump back in 2020 with the twins, a lot of people in the BASE community know exactly who I am talking about lmfao. I hate hiking, but I have to say that this hike was actually pretty rad. The jump was amazing too, it was beautiful and  a very long flight!',
+	created_at: 'January 12, 2023',
+},
+{
+	username: 'jacksonboyett',
+	text: 'This jump is amazing!',
+	created_at: 'January 12, 2023',
+}
 ]
