0cb6c5d452cda1fb8569757e078d2881362281d1
Luke Boyett <lboyett@gmail.com>
2023-05-21 17:42:25 +0200

    fix cors 2

--- server/index.ts
+++ server/index.ts
@@ -18,7 +18,7 @@ const app = express();
 const port = 8000;
 const corsOptions = {
   credentials: true,
-  origin: process.env.CLIENT_DOMAIN,
+  origin: "https://www.exit-map.com",
 };
 
 app.use(cors(corsOptions));
