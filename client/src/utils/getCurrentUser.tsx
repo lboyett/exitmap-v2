c96174ac97a90aa5ac3d0009642af417065f59b7
Luke Boyett <lboyett@gmail.com>
2023-05-25 16:25:53 +0200

    fix url issue

--- client/src/utils/getCurrentUser.tsx
+++ client/src/utils/getCurrentUser.tsx
@@ -5,6 +5,7 @@ import { UserContext } from "../context/UserContext";
 export default async function getCurrentUser() {
   return new Promise(async (resolve, reject) => {
     try {
+      console.log(import.meta.env.VITE_SERVER_DOMAIN);
       const response = await axios.get(
         `${import.meta.env.VITE_SERVER_DOMAIN}/users/current-user`,
         {
