e3442c975d3fa677f201cf48eb93935fa0a4ffdb
jacksonboyett <jackson.boyett@gmail.com>
2023-05-26 11:33:50 +0200

    added navigate to home from domain page

--- client/src/pages/domain/Domain.tsx
+++ client/src/pages/domain/Domain.tsx
@@ -1,11 +1,14 @@
 import { UserContext } from "../../context/UserContext";
 import { useContext } from "react";
 import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
 function Domain() {
 	const [userContext] = useContext(UserContext)
+	const navigate = useNavigate();
 useEffect(() => {
 	console.log(userContext)
+	navigate("/home");
 }, [userContext]);
 
 	return ( <div className="domain">Domain</div> );
